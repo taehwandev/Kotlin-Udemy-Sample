f7207f78d9441d079964b6806ba4cb7496415960
TaeHwan <taehwan@thdev.net>
2016-10-04 23:23:13 +0900

    MainActivity 위치 변경으로 인한 오류 패치

--- app/src/main/java/tech/thdev/kotlin_udemy_sample/view/main/MainFragment.kt
+++ app/src/main/java/tech/thdev/kotlin_udemy_sample/view/main/MainFragment.kt
@@ -18,7 +18,7 @@ class MainFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View?
-        = inflater?.inflate(R.layout.fragment_main, container, false)
+            = inflater?.inflate(R.layout.fragment_main, container, false)
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
