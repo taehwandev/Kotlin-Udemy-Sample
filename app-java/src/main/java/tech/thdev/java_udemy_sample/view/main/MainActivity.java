cc695befbbd2df55df3d8c00b2b9bd066ad0e0bd
TaeHwan <taehwan@thdev.net>
2016-10-05 22:15:16 +0900

    Java 예제를 추가  - Java로 작성한 부분과 코틀린 부분을 비교하기 위하여 Java app 추가
    Kotlin 예제 수정
     - Kotlin의 레이아웃 수정
     - Java 예제 추가로 app-kotlin으로 이름 변경
    
    dependencies.gradle을 추가
     - Android support library에 대한 버전 분기
     - App 버전 분기

---
+++ app-java/src/main/java/tech/thdev/java_udemy_sample/view/main/MainActivity.java
@@ -0,0 +1,45 @@
+package tech.thdev.java_udemy_sample.view.main;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import tech.thdev.java_udemy_sample.R;
+import tech.thdev.java_udemy_sample.util.ActivityUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ButterKnife 사용
+        ButterKnife.bind(this);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // static Util을 이용하여 replace 처리
+        ActivityUtil.replaceFragmentToActivity(getSupportFragmentManager(), MainFragment.getInstance(), R.id.frame_layout);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
