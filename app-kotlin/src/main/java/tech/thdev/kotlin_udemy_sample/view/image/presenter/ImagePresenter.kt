c189ec6df5fb5304b42514e1f4e52817d6ee2799
TaeHwan <taehwan@thdev.net>
2016-11-08 00:56:16 +0900

    #12 예외처리 추가

--- app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenter.kt
+++ app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenter.kt
@@ -17,25 +17,28 @@ class ImagePresenter : ImageContract.Presenter {
 
     override var photoDataSample: PhotoDataSource? = null
 
-    var page = -1
+    var page = 0
 
     override fun getRecentImageSample() {
         // object callback을 정의한다
-        photoDataSample?.getRecentPhoto(page)
+        photoDataSample?.getRecentPhoto(++page)
                 ?.enqueue(object : Callback<PhotoResponse> {
 
                     override fun onResponse(call: Call<PhotoResponse>?, response: Response<PhotoResponse>?) {
-                        response?.isSuccessful.apply {
+                        if (response?.isSuccessful ?: false) {
+                            Log.d("TAG", "response raw " + response?.raw())
+
                             val photoResponse = response?.body()
                             if (photoResponse?.stat === "ok") {
-                                Log.d("TAG", "response raw " + response?.raw())
                                 view?.showLoadSuccess()
 
                             } else {
                                 view?.showLoadFailMessage("Code ${photoResponse?.code}, message ${photoResponse?.message}")
                             }
 
-                        } ?: view?.showLoadFail()
+                        } else {
+                            view?.showLoadFail()
+                        }
                     }
 
                     override fun onFailure(call: Call<PhotoResponse>?, t: Throwable?) {
