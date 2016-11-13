0fac87e2420eab52c7bbc6de66f5652b73cc8e29
TaeHwan <taehwan@thdev.net>
2016-11-13 13:20:18 +0900

    #12 TDD 코드 일부 제거

--- app-kotlin/src/test/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenterTest.kt
+++ app-kotlin/src/test/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenterTest.kt
@@ -41,9 +41,6 @@ class ImagePresenterTest {
             }
         }
 
-        // TODO 차례대로 테스트를 진행해보세요
         verify(view).showLoadSuccess()
-//        verify(view).showLoadFail()
-//        verify(view).showLoadFailMessage("Error")
     }
 }
