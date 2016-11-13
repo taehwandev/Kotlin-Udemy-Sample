a47116fc7b3e094e60eb1bf4ce7f2620891a9f8e
TaeHwan <taehwan@thdev.net>
2016-11-13 11:57:19 +0900

    #10 TDD 코드 추가

--- app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenter.kt
+++ app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/image/presenter/ImagePresenter.kt
@@ -1,6 +1,5 @@
 package tech.thdev.kotlin_udemy_sample.view.image.presenter
 
-import android.util.Log
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,7 +25,7 @@ class ImagePresenter : ImageContract.Presenter {
 
                     override fun onResponse(call: Call<PhotoResponse>?, response: Response<PhotoResponse>?) {
                         if (response?.isSuccessful ?: false) {
-                            Log.d("TAG", "response raw " + response?.raw())
+//                            Log.d("TAG", "response raw " + response?.raw())
 
                             val photoResponse = response?.body()
                             if (photoResponse?.stat.equals("ok")) {
