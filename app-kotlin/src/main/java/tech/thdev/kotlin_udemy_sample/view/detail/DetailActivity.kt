f46fe77283b5e15c521ce830be6e215559e20889
TaeHwan <taehwan@thdev.net>
2016-11-15 23:11:18 +0900

    #17 - BottomSheet 추가

--- app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/detail/DetailActivity.kt
+++ app-kotlin/src/main/java/tech/thdev/kotlin_udemy_sample/view/detail/DetailActivity.kt
@@ -11,6 +11,8 @@ import tech.thdev.kotlin_udemy_sample.R
 import tech.thdev.kotlin_udemy_sample.constant.Constant
 import tech.thdev.kotlin_udemy_sample.data.FlickrPhoto
 import tech.thdev.kotlin_udemy_sample.data.model.PhotoDataSource
+import tech.thdev.kotlin_udemy_sample.util.decimalFormat
+import tech.thdev.kotlin_udemy_sample.util.getDate
 import tech.thdev.kotlin_udemy_sample.view.detail.presenter.DetailContract
 import tech.thdev.kotlin_udemy_sample.view.detail.presenter.DetailPresenter
 
@@ -45,9 +47,9 @@ class DetailActivity : BasePresenterActivity<DetailContract.View, DetailContract
 
         tv_title.text = photo.title.toString()
         tv_content.text = Html.fromHtml(photo.description._content)
-        tv_view_count.text = photo.views
-        tv_date.text = photo.dates.lastupdate
-        tv_comment_count.text = photo.comments._content
+        tv_date.text = photo.dates.lastupdate.getDate("MM-dd-yyyy")
+        tv_viewer_count.text = photo.views.decimalFormat()
+        tv_comment_count.text = photo.comments._content.decimalFormat()
 
         tv_user_name.text = photo.owner.username
     }
