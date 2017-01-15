3d75d6365f411312643d5a6190f569d878242f80
TaeHwan <taehwan@thdev.net>
2017-01-15 23:54:33 +0900

    Update kotlin version  1.0.5 버전 이후에 맞게 코드 수정

--- app-kotlin/src/test/java/tech/thdev/kotlin_udemy_sample/null_safety_02/_04_NullPointExceptionCatchTest.kt
+++ app-kotlin/src/test/java/tech/thdev/kotlin_udemy_sample/null_safety_02/_04_NullPointExceptionCatchTest.kt
@@ -9,26 +9,6 @@ import org.junit.Test
  */
 class _04_NullPointExceptionCatchTest {
 
-    /**
-     * try/catch를 이용한 예외처리
-     */
-    @Test
-    @Throws(InterruptedException::class)
-    fun test() {
-        val temp: String? = null
-        val size: Int
-        try {
-            size = temp!!.length
-        } catch (e: Exception) {
-            size = 0
-        }
-        print(size)
-    }
-
-    /**
-     * 1.0.4 버전 부터는 다음과 같이 try / catch를 사용하여야 한다.
-     * 1.0.5 부터는 아래와 같지 않으면 오류 발생
-     */
     @Test
     @Throws(InterruptedException::class)
     fun test2() {
