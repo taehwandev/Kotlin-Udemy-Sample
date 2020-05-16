18b759e33e699e6f8e5fcbc47e55683722507887
Taehwan <taehwan@thdev.net>
2020-05-16 17:01:13 +0900

    #60 Update library new - Renewal - Bottom Sheet

--- bottomsheetsample/src/main/java/tech/thdev/bottomsheetsample/MainActivity.kt
+++ bottomsheetsample/src/main/java/tech/thdev/bottomsheetsample/MainActivity.kt
@@ -1,15 +1,20 @@
 package tech.thdev.bottomsheetsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomSheetBehavior
 import android.util.Log
 import android.util.TypedValue
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // rl_bottom_sheet을 BottomSheetBehavior에 등록한다
+    private val bottomSheetBehavior by lazy {
+        BottomSheetBehavior.from(rl_bottom_sheet)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,32 +22,38 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-        // rl_bottom_sheet을 BottomSheetBehavior에 등록한다
-        val bottomSheetBehavior = BottomSheetBehavior.from(rl_bottom_sheet)
         // 높이 설정
         bottomSheetBehavior.peekHeight = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, 200f, resources.displayMetrics).toInt()
+            TypedValue.COMPLEX_UNIT_DIP, 200f, resources.displayMetrics
+        ).toInt()
 
-        bottomSheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                // Slide ...
-                // 1이면 완전 펼쳐진 상태
-                // 0이면 peekHeight인 상태
-                // -1이면 숨김 상태
-                Log.i("TAG", "slideOffset $slideOffset")
-            }
-
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                Log.d("TAG", "newState $newState")
-            }
-        })
+        bottomSheetBehavior.addBottomSheetCallback(bottomSheetCallback)
 
         btn_show.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
         }
         btn_hide.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bottomSheetBehavior.removeBottomSheetCallback(bottomSheetCallback)
+    }
+
+    private val bottomSheetCallback = object : BottomSheetBehavior.BottomSheetCallback() {
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            // Slide ...
+            // 1이면 완전 펼쳐진 상태
+            // 0이면 peekHeight인 상태
+            // -1이면 숨김 상태
+            Log.i("TAG", "slideOffset $slideOffset")
+        }
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            Log.d("TAG", "newState $newState")
         }
     }
 }
