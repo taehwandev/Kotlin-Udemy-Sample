bbc264597ceb0fe5e1492e9d1de44177d9688adf
taehwan <taehwan@thdev.net>
2024-02-11 16:35:28 +0900

    no39 - 코드 최신화

--- app/src/main/java/tech/thdev/app/view/main/home/HomeFragment.kt
+++ app/src/main/java/tech/thdev/app/view/main/home/HomeFragment.kt
@@ -7,9 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_home.*
-import tech.thdev.app.R
 import tech.thdev.app.data.source.image.ImageRepository
+import tech.thdev.app.databinding.FragmentHomeBinding
 import tech.thdev.app.view.main.home.adapter.ImageRecyclerAdapter
 import tech.thdev.app.view.main.home.presenter.HomeContract
 import tech.thdev.app.view.main.home.presenter.HomePresenter
@@ -28,15 +27,23 @@ class HomeFragment : Fragment(), HomeContract.View {
         ImageRecyclerAdapter()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_home, container, false)
+    private lateinit var fragmentHomeBinding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
+        FragmentHomeBinding.inflate(inflater, container, false).also {
+            fragmentHomeBinding = it
+        }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         homePresenter.loadImage()
 
-        recycler_view.run {
+        fragmentHomeBinding.recyclerView.run {
             adapter = imageRecyclerAdapter
             layoutManager = GridLayoutManager(this@HomeFragment.context, 3)
             addOnScrollListener(recyclerViewOnScrollListener)
@@ -45,15 +52,15 @@ class HomeFragment : Fragment(), HomeContract.View {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        recycler_view?.removeOnScrollListener(recyclerViewOnScrollListener)
+        fragmentHomeBinding.recyclerView.removeOnScrollListener(recyclerViewOnScrollListener)
     }
 
     override fun hideProgress() {
-        progressBar.visibility = View.GONE
+        fragmentHomeBinding.progressBar.visibility = View.GONE
     }
 
     override fun showProgress() {
-        progressBar.visibility = View.VISIBLE
+        fragmentHomeBinding.progressBar.visibility = View.VISIBLE
     }
 
     private val recyclerViewOnScrollListener = object : RecyclerView.OnScrollListener() {
@@ -63,7 +70,8 @@ class HomeFragment : Fragment(), HomeContract.View {
 
             val visibleItemCount = recyclerView.childCount
             val totalItemCount = imageRecyclerAdapter.itemCount
-            val firstVisibleItem = (recyclerView.layoutManager as? GridLayoutManager)?.findFirstVisibleItemPosition()
+            val firstVisibleItem =
+                (recyclerView.layoutManager as? GridLayoutManager)?.findFirstVisibleItemPosition()
                     ?: 0
 
             if (!homePresenter.isLoading && (firstVisibleItem + visibleItemCount) >= totalItemCount - 7) {
--- app/src/main/java/tech/thdev/app/view/main/home/adapter/holder/ImageViewHolder.kt
+++ app/src/main/java/tech/thdev/app/view/main/home/adapter/holder/ImageViewHolder.kt
@@ -3,8 +3,9 @@ package tech.thdev.app.view.main.home.adapter.holder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_image_view.view.*
 import tech.thdev.app.R
 import tech.thdev.app.data.ImageData
 
@@ -15,13 +16,21 @@ class ImageViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_image_view, parent, false)
 ) {
 
+    private val tvTitle: TextView by lazy {
+        itemView.findViewById(R.id.tv_title)
+    }
+
+    private val imgView: ImageView by lazy {
+        itemView.findViewById(R.id.img_view)
+    }
+
     fun onBind(item: ImageData) {
         itemView.onBind(item)
     }
 
     private fun View.onBind(item: ImageData) {
-        tv_title.text = item.name
-        img_view.setImageResource(
+        tvTitle.text = item.name
+        imgView.setImageResource(
             resources.getIdentifier(
                 item.fileName,
                 "drawable",
